f9aa227e9e3135011557d6fb4c3b9fdb1bbd493a
Diego Rodríguez <diego@rodribus.com>
2016-02-10 21:27:57 +0100

    Changed Mocha tests to run through gulp
    - Removed Makefile
    - Added test task to gulp
    
    Also removed annoying notify module.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
-    notify = require('gulp-notify');
+    mocha = require('gulp-mocha');
 
 gulp.task('hint', function(){
     return gulp.src(['index.js', 'lib/*.js'])
     .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(notify({
-        "title": "Gulp!",
-        "message": "Executed tasks."
-      }));
+    .pipe(jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('watch', ['hint'], function(){
+gulp.task('test', function(){
+    return gulp.src('test/test.js', {read: false})
+        .pipe(mocha());
+});
+
+gulp.task('watch', ['hint', 'test'], function(){
     gulp.watch(['index.js', 'lib/*.js'], ['default']);
+    gulp.watch(['test/test.js'], ['test']);
 });
 
 gulp.task('default', ['watch']);
