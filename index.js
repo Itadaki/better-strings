f41552ee9019702e277add6c6d211fba939fbd2b
Diego Rodríguez <diego@rodribus.com>
2016-02-11 20:09:53 +0100

    Now the module exports a 'format' function.
    Two ways of use format module:
    
     - using format as string method
     - using format as function

--- index.js
+++ index.js
@@ -2,26 +2,41 @@
 
 var check = require('./lib/check-allowed-types.js');
 
+//expect to receive allowed types args
+var format = function () {
+    var args = Array.prototype.slice.call( arguments, 0 );
+    var base = args.splice(0, 1)[0];
+    if (typeof base !== 'string' || !check.apply( this, args )){
+        throw new TypeError('One of the arguments suplied is not from an allowed type');
+    }
+
+    //replace placeholders with args
+    args.forEach(function(element, index, arr){
+        var exp = '\{('+index+')\}';
+        var re = new RegExp(exp, 'g');
+        base = base.replace(re, element);
+    });
+    return base;
+};
+
 //add function to prototype
 if (!String.prototype.format){
-    String.prototype.format = function () {
-        var base = this.toString();
+    String.prototype.format = function(){
         var args = Array.prototype.slice.call( arguments, 0 );
-
-        if (!check.apply( this, args )){
-            throw new TypeError('One of the arguments suplied is not from an allowed type');
-        }
-
-        //replace placeholders with args
-        args.forEach(function(element, index, arr){
-            var exp = '\{('+index+')\}';
-            var re = new RegExp(exp, 'g');
-            base = base.replace(re, element);
-        });
-        return base;
+        args.unshift(this.toString());
+        return format.apply(this, args);
     };
 }
 
-module.exports = function(){
-
+module.exports = {
+    //expects to take at least 2 args, first a string-type
+    format: function(){
+        var args = Array.prototype.slice.call( arguments, 0 );
+        var base = args[0];
+        if(typeof base === 'string'){
+            return format.apply( this, args );
+        } else {
+            throw new TypeError('First argument is not an string!');
+        }
+    }
 };
